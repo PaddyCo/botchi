739c8a65e2a6fedb2237574a56501a40e15eabf8
Svampson <patrik@damn.moe>
2017-10-12 23:08:00 +0200

    Saved messages in db for markov stuff

--- modules/markov.js
+++ modules/markov.js
@@ -1,12 +1,16 @@
 import logger from "../logger";
-import util from "util";
 import { sendCaption } from "../captionGenerator";
 import markov from "markov";
+import sqlite3 from "sqlite3";
 
 class Markov {
   constructor(client) {
     this.client = client;
     this.markovChains = {};
+
+    // Initialize the DB
+    this.db = new sqlite3.Database("database.sqlite");
+    this.db.run("CREATE TABLE IF NOT EXISTS messages (message TEXT, author_id TEXT, channel_id TEXT)");
 
     const channels = client.channels.array().filter((c) => c.type == "text");
     logger.log("info", `Initializing markov chains for ${channels.length} channels`);
@@ -17,7 +21,7 @@ class Markov {
     client.on("message", (msg) => {
       if (msg.isMemberMentioned(this.client.user)) {
         const markovChain = this.markovChains[msg.channel.id];
-        const message = markovChain.respond(msg.cleanContent, 10).join(" ");
+        const message = markovChain.respond(msg.cleanContent, 5).join(" ");
         sendCaption(msg.channel, message, null);
       }
     });
@@ -31,13 +35,6 @@ class Markov {
       }
     });
 
-    client.on("message", (msg) => {
-      if (msg.content == "!markov") {
-        const markovChain = this.markovChains[msg.channel.id];
-        const message = markovChain.fill(markovChain.pick(), 10).join(" ");
-        sendCaption(msg.channel, message, null);
-      }
-    });
 
     logger.log("info", "Markov module initialized successfully");
   }
@@ -45,18 +42,13 @@ class Markov {
 
   initializeMarkovChain(channel) {
     logger.log("info", `Initializing markov chain for ${channel.name}`)
-    const markovChain = new markov(2);
-    return channel.fetchMessages({ limit: 100 })
-      .then(messages => {
-        const messagesArray = messages.array();
-        for (let i = 0; i < messagesArray.length; i++) {
-          if (!messagesArray[i].author.bot && !messagesArray[i].isMemberMentioned(this.client.user)) {
-            markovChain.seed(messagesArray[i].cleanContent);
-          }
-        }
-        this.markovChains[channel.id] = markovChain;
-        return markovChain;
-      });
+    this.markovChains[channel.id] = new markov(3);
+
+    this.db.each(`SELECT message FROM messages WHERE channel_id = "${channel.id}"`, (err, row) => {
+      this.markovChains[channel.id].seed(row.message);
+    }, () => {
+      logger.info("info", `Finished loading messages for markov chain for ${channel.name}`)
+    });
   }
 }
 
